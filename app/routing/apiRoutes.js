a0f9a21367b2f6d646e879a7a2677fe2965a98d8
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-10-07 15:23:46 -0400

    homework startup

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+var friends = require("../data/friends");
+
+module.exports = function(app){
+    app.get("/api/friends", function(rep, res){
+        
+    })
+    app.post("/api/friends", function(rep, res){
+
+    })
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+var path= require("path");
+
+module.exports = function(app){
+   app.get("/", function(req,res){
+       
+   });
+   app.get("/survey", function(req,res){
+
+   });
+   app.get("*", function(req,res){
+
+   });
+}
