edb5e60c2e33fc8f571cb85095b4c7403dc13b33
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-10-07 17:18:06 -0400

    homework startup

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var friends = require("../data/friends");
 
 module.exports = function(app){
     app.get("/api/friends", function(rep, res){
-        
+        res.json(friends)
     })
     app.post("/api/friends", function(rep, res){
 
