dd74b9668367fe8259f528aa3e64ab68fe522e24
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-10-09 04:17:51 -0400

    updated friends arr, added additonal friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,37 +2,35 @@
 
 var friends = [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "name":"DMX",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpvJ_leItmg9W9EqNmq9mXu_9Rcn-ZgIC08vu66vdCBVEsmmAI&s",
+    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+    "name":"Busta-Rhymes",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4D7il-26FQ4IWkPjSXbHSh54MEPI96GIW-TAIvmHp3vYKWUrv&s",
+    "scores":[4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+    {
+    "name":"Snoop Dogg",
+    "photo":"https://m.media-amazon.com/images/M/MV5BMjE2OTUwNTk4NF5BMl5BanBnXkFtZTcwMjMwOTk0NA@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+    "scores":[5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+    {
+    "name":"Ice Cube",
+    "photo":"https://static.hiphopdx.com/2015/06/Ice-Cube_06-08-2015.jpg",
+    "scores":[3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+    {
+    "name":"Nas",
+    "photo":"http://images.nymag.com/guides/summer/2008/nas080630_250x375.jpg",
+    "scores":[4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+    {
+    "name":"50 Cent",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/50_Cent.jpg/220px-50_Cent.jpg",
+    "scores":[4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+  },
 ];
 
   module.exports = friends;
